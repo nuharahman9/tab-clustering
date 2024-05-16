fe28a655402c3ca72a697c59bdb40affdf41b09b
nuharahman9 <nrahm009@ucr.edu>
2024-05-16 13:02:48 -0700

    event listener

--- js/popup.js
+++ js/popup.js
@@ -16,24 +16,31 @@ function extractTabText(tabId) {
     }); 
 }
   
-  // Listener for extract button click
 
-document.getElementById('textCapture').addEventListener('click', () => {
-    // Get current active tab
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-        const promises = tabs.map(tab => { 
-            return new Promise((resolve, reject) => { 
-                extractTabText(tab.id)
-                .then(result => resolve(result))
-                .catch(err => reject(err))
-            }); 
-        }); 
-    }); 
-        // Send message to the background script to initiate text extraction
-    Promise.all(promises)
-        .then(results => {
-            console.log(results)})
-        .catch(error => {
-            console.error('Error extracting text from tabs:', error)});
+window.addEventListener('DOMContentLoaded', function() {
+    var myBtn = document.getElementById('textCapture');
+    console.log(myBtn);  
+
 }); 
+
+
+
+// document.getElementById("textCapture").addEventListener('click', () => {
+//     console.log(document.getElementById("textCapture"))
+//     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+//         const promises = tabs.map(tab => { 
+//             return new Promise((resolve, reject) => { 
+//                 extractTabText(tab.id)
+//                 .then(result => resolve(result))
+//                 .catch(err => reject(err))
+//             }); 
+//         }); 
+//     }); 
+
+//     Promise.all(promises)
+//         .then(results => {
+//             console.log(results)})
+//         .catch(error => {
+//             console.error('Error extracting text from tabs:', error)});
+// }); 
   
