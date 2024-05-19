9073b61d6cb610c3382a9e13101dcd7efa0352c0
nuharahman9 <nrahm009@ucr.edu>
2024-05-19 14:42:45 -0700

    confusion

--- js/bg.js
+++ js/bg.js
@@ -1,29 +1,19 @@
-
 function getTextContent() { 
-    const fs = require('fs'); 
-    const output = document.all[0].innerText; 
-    fs.writeFile('output.txt', output, (err) => { 
-        if (err) throw err; 
-    }); 
-    return output; 
+    console.log("i am getting de text"); 
+    return document.body.innerText; 
 }
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     if (message === 'getText') { 
         console.log("background"); 
-        chrome.tabs.query({ currentWindow: true}, (tabs) => { 
-            tabs.forEach((tab) => { 
+        chrome.tabs.query({ currentWindow: true }, tabs => { 
+            tabs.forEach(tab => {
                 chrome.scripting.executeScript({
-                    target: { tabId: tab.id }, 
-                    function: getTextContent
-                }, (results) => {
-                    if (result && results[0]) {
-                        console.log(results); 
-                    } else { 
-                        console.log("err"); 
-                    }
-
-                })
+                    target : { tabId: tab.id }, 
+                    func : getTextContent
+                }).then(() => { 
+                    console.log("success"); 
+                }); 
 
             }); 
 
