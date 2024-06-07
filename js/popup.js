8a72f7cffe49f24cec41bdf47d57b83d3e243682
nuharahman9 <nrahm009@ucr.edu>
2024-06-07 11:55:27 -0700

    scroll button slideshow

--- js/popup.js
+++ js/popup.js
@@ -1,4 +1,31 @@
+var slideIdx = 1 
+showDiv(1)
+
+document.getElementById('left-scroll').addEventListener('click', () => moveDiv(-1)); 
+document.getElementById('right-scroll').addEventListener('click', () => moveDiv(+1))
+
+function moveDiv(n) { 
+    showDiv(slideIdx += n)
+}
+
+
+function showDiv(n) { 
+    var i 
+    var x = document.getElementsByClassName("slide")
+    if (n > x.length) { slideIdx = 1 }
+    if (n < 1) { slideIdx = x.length }
+    for (i = 0; i < x.length; i++) { 
+        x[i].style.display = "none"; 
+    }
+
+    x[slideIdx-1].style.display =  "block"
+}
+
+
+
+
 async function getNumWindows() { 
+    console.log("get num windows triggered")
     await chrome.tabs.query({ currentWindow: true }).then(tabs => { 
         tabLen = tabs.length 
         $(document).ready(function() {
@@ -19,7 +46,7 @@ let options = {
 
 let observer = new IntersectionObserver(getNumWindows, options)
 
-let target = document.querySelector("#popup-window"); 
+let target = document.querySelector("#numwindows"); 
 console.log(target)
 observer.observe(target); 
 
