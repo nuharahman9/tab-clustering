9f6d352e39df0561ed2e54321dd2d6734d5f5996
nuharahman9 <nrahm009@ucr.edu>
2024-06-14 14:59:20 -0700

    testing and plotting

--- websiteTopicModel.py
+++ websiteTopicModel.py
@@ -30,13 +30,12 @@ class websiteTopicModel:
 
     def __init__(self, n_components): 
         self.n_components = n_components
-        self.vectorizer = TfidfVectorizer(norm='l2', smooth_idf=True, max_features=5000, max_df=0.95, ngram_range=(2, 3))
+        self.vectorizer = TfidfVectorizer(norm='l2', smooth_idf=True, max_df=0.95, ngram_range=(2, 3), stop_words='english')
         self.file_paths = []
         self.tfidf_matrix = None 
         self.tokens = None 
         self.W = None 
         self.ps = PorterStemmer()
-        self.stopwords = set(stopwords.words('english'))
         self.H = None 
         self.websites_preprocessed_data = [] 
         self.topics = [] 
@@ -59,23 +58,21 @@ class websiteTopicModel:
             text = text.strip()
             text = text.lower()
             tokenized_words = word_tokenize(text)
-            tokenized_words = [w for w in tokenized_words if w not in self.stopwords]
             tokenized_words = [self.ps.stem(word) for word in tokenized_words]
             self.websites_preprocessed_data.append(' '.join(tokenized_words)) 
         print("finished processing text\n")
         
     
     def create_tfidf_matrix(self): 
-        print("entering generate_nmf_model\n")
         self.tfidf_matrix = self.vectorizer.fit_transform(self.websites_preprocessed_data)
-        print(type(self.tfidf_matrix))
         print("vectorizer created\n")
     
     def generate_nmf_model(self): 
+        print("entering generate_nmf_model\n")
         if (self.n_components == -1): 
             self.approximate_best_n()
         else: 
-            self.nmf_model = NMF(n_components=self.n_components, random_state=1, max_iter=150, solver='mu')
+            self.nmf_model = NMF(n_components=self.n_components, random_state=1,  solver='mu')
             self.W = self.nmf_model.fit_transform(self.tfidf_matrix)
             self.H = self.nmf_model.components_ 
     
@@ -95,22 +92,26 @@ class websiteTopicModel:
         best_num_topics = 0 
         best_coherence_score = float('-inf')
         min = 2
+        x = [2, 3, 4, 5, 6, 7, 8, 9]
+        y = []
         print("approximate_best_n: ", min, max)
         for i in range(min, max): # dont wanna have a window with just one page - open for suggestion on this one ?
             curr_topics = [] 
-            self.nmf_model = NMF(n_components=i, random_state=60, solver='mu')
+            self.nmf_model = NMF(n_components=i, random_state=60, solver='mu', init='nndsvda')
             self.W = self.nmf_model.fit_transform(self.tfidf_matrix)
             self.H = self.nmf_model.components_  
             curr_topics = self.get_topics()
             cm = CoherenceModel(topics=curr_topics, texts=word_lists, dictionary=dict, corpus=corpus, coherence='c_uci')
             curr_coherence = round(cm.get_coherence(), 5)
             print('(', i, ',', curr_coherence, ')')
+            y.append(curr_coherence)
             if (curr_coherence > best_coherence_score): 
                 best_num_topics = i 
                 best_coherence_score = curr_coherence 
 
+
         print("best number of components: ", best_num_topics) 
-        self.nmf_model = NMF(n_components=best_num_topics, random_state=60, solver='mu')
+        self.nmf_model = NMF(n_components=best_num_topics, random_state=1, solver='mu')
         self.W = self.nmf_model.fit_transform(self.tfidf_matrix)
         self.H = self.nmf_model.components_  
 
@@ -146,28 +147,10 @@ class websiteTopicModel:
 
 
 
-# def main(): 
-#     file_paths = load_corpus('./corpus')
-#     websites_preprocessed_data = [] 
-#     for path in file_paths: 
-#         websites_preprocessed_data.append(read_txt(path))
-    
-#     vectorizer = TfidfVectorizer(norm='l2', smooth_idf=True, ngram_range=(2, 3))
-#     x = vectorizer.fit_transform(websites_preprocessed_data)
-
-#     nmf_model = NMF(n_components=4, random_state=60)
-#     W = nmf_model.fit_transform(x)
-#     H = nmf_model.components_ 
-
-#     terms = vectorizer.get_feature_names_out()
-#     topics = [] 
-
-#     for index, topic in enumerate(H):
-#         topics.append([terms[i] for i in topic.argsort()[-3:]])
-#         print([terms[i] for i in topic.argsort()[-4:]])
+def main(): 
+    nmf_model = websiteTopicModel(n_components=-1)
+    return nmf_model.driver() 
 
 
-#     print(W)
-
-
-        
+if __name__ == '__main__':
+    topic_doc_map = main()
